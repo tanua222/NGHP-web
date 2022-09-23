4cb49f0d21ec15290a68604cae1038cb72ad7ea3
Karamveer Singh <x245072@corp.ads>
2022-09-23 10:39:46 -0700

    refactored pre-check

--- src/components/PreCheck/PreCheck.js
+++ src/components/PreCheck/PreCheck.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import BlifTypography from '../Common/BlifTypography/BlifTypography';
 import BlifStackView from '../Common/BlifStackView/BlifStackView';
+import {ARRAY_MAP_KEYS, FILTER_TYPES} from '../../utils/commonKeys';
+import {FiltersView} from '../Common/Filters/Filters';
 
 import BlifBox from '../Common/Box/BlifBox';
 import {
@@ -10,7 +12,6 @@ import {
 } from '../Common/BlifFlexGrid/BlifFlexGrid';
 import {Spacer} from '@telus-uds/ds-allium';
 
-import PreCheckFilters from './PreCheckFilters';
 // OPTIONS
 export const PROVINCE_FILTER_OPTIONS = [
     {text: 'ALL', value: 'ALL'},
@@ -44,6 +45,35 @@ const FORM_SCHMEA = {
     [FORM_SCHEMA_KEYS.FILE_INPUT]: '',
 };
 
+//OPTIONS
+
+const FILTER_SCHEMA = [
+    {
+        [ARRAY_MAP_KEYS.FILTER_TYPE]: FILTER_TYPES.TEXT_INPUT,
+        [ARRAY_MAP_KEYS.LABEL]: 'File',
+    },
+    {
+        [ARRAY_MAP_KEYS.FILTER_TYPE]: FILTER_TYPES.SELECT_INPUT,
+        [ARRAY_MAP_KEYS.LABEL]: 'Province',
+        [ARRAY_MAP_KEYS.OPTIONS]: PROVINCE_FILTER_OPTIONS,
+    },
+    {
+        [ARRAY_MAP_KEYS.FILTER_TYPE]: FILTER_TYPES.SELECT_INPUT,
+        [ARRAY_MAP_KEYS.LABEL]: 'CLEC',
+        [ARRAY_MAP_KEYS.OPTIONS]: CLEC_FILTER_OPTIONS,
+    },
+    {
+        [ARRAY_MAP_KEYS.FILTER_TYPE]: FILTER_TYPES.SELECT_INPUT,
+        [ARRAY_MAP_KEYS.LABEL]: 'BRG Indicator',
+        [ARRAY_MAP_KEYS.OPTIONS]: BRG_INDICATOR_FILTER_OPTIONS,
+    },
+    {
+        [ARRAY_MAP_KEYS.FILTER_TYPE]: FILTER_TYPES.SELECT_INPUT,
+        [ARRAY_MAP_KEYS.LABEL]: 'Rejects',
+        [ARRAY_MAP_KEYS.OPTIONS]: REJECTS_FILTER_OPTIONS,
+    },
+];
+
 // search onClick handler
 const clickHandler = (e) => {
     e.preventDefault();
@@ -72,7 +102,10 @@ const PreCheck = () => {
                         <BlifFlexGridRow
                             verticalAlign="center"
                             horizontalAlign="center">
-                            <PreCheckFilters clickHandler={clickHandler} />
+                            <FiltersView
+                                clickHandler={clickHandler}
+                                schema={FILTER_SCHEMA}
+                            />
                         </BlifFlexGridRow>
                     </BlifBox>
                 </BlifFlexGridCol>
